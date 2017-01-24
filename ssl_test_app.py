d2878319637b460175282ee9ab4f848d5422cd16
David Wahlund <david.wahlund@mellanskog.se>
2017-01-24 01:01:03 +0100

    Code refactor More TLSA info

--- ssl_test_app.py
+++ ssl_test_app.py
@@ -64,38 +64,23 @@ def overview():
 
 
 @app.route('/api/https/<host>')
-@as_json
-def api_https(host):
-    return _make_https_result(host)
-
-
 @app.route('/api/https/<host>/<int:port>')
 @as_json
-def api_https_port(host, port):
+def api_https_port(host, port=443):
     return _make_https_result(host, port)
 
 
 @app.route('/api/smtp/<host>')
-@as_json
-def api_smtp(host):
-    return _make_smtp_result(host)
-
-
 @app.route('/api/smtp/<host>/<int:port>')
 @as_json
-def api_smtp_port(host, port=None):
+def api_smtp_port(host, port=25):
     return _make_smtp_result(host, port)
 
 
 @app.route('/api/sshfp/<host>')
-@as_json
-def api_sshfp(host):
-    return _make_sshfp_result(host)
-
-
 @app.route('/api/sshfp/<host>/<int:port>')
 @as_json
-def api_sshfp_port(host, port=None):
+def api_sshfp_port(host, port=22):
     return _make_sshfp_result(host, port)
 
 
@@ -147,85 +132,36 @@ def sshfp():
     return render_template('sshfp.html', form=form, result=result)
 
 
-def _get_context():
+def _get_resolver():
+    r = dns.resolver.Resolver()
+    r.nameservers = ['8.8.8.8']
+    r.use_edns(0, dns.flags.DO, 1280)
+    return r
+
+
+def _get_context(check_hostname=False):
     # return ssl.create_default_context(cafile='ca/ca-sha2.pem')
-    return ssl.create_default_context()
-
-def _get_field(s, d):
-    for t in d:
-        if t[0][0] == s:
-            return t[0][1]
+    c = ssl.create_default_context()
+    c.check_hostname = check_hostname
+    c.verify_mode = ssl.CERT_OPTIONAL
 
 
-def _make_https_result(host, port=443):
-    try:
-        c, dc = get_cert(host, port)
-    except (socket.error, TimeoutError, ssl.SSLError, ssl.CertificateError, dns.exception.DNSException) as e:
-        return {'host': host, 'port': port, 'error': str(e)}
-
-    return _make_tlsa_result(host, port, c, dc)
+    return c
 
 
-def _make_smtp_result(host, port=25):
-    try:
-        c, dc = get_cert_smtp(host, port)
-    except (socket.error, TimeoutError, ssl.SSLError, ssl.CertificateError, dns.exception.DNSException) as e:
-        return {'host': host, 'port': port, 'error': str(e)}
-
-    return _make_tlsa_result(host, port, c, dc)
-
-
-def _make_sshfp_result(host, port=22):
-    try:
-        result = dict()
-        result['host'] = host
-        result['port'] = port
-        k = _get_ssh_key(host, port)
-        sshfp = _get_sshfp(host)
-        result['sshfp'] = sshfp
-        result['check'] = _check_sshfp(k, sshfp)
-        return result
-    except (TimeoutError, dns.exception.DNSException) as e:
-        return {'host': host, 'port': port, 'error': str(e)}
+def _get_cert(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_context = _get_context(True)
+    # ssl_context.check_hostname = True
+    ssl_sock = ssl_context.wrap_socket(s, server_hostname=host)
+    ssl_sock.connect((host, port))
+    c = ssl_sock.getpeercert()
+    dc = ssl_sock.getpeercert(binary_form=True)
+    ssl_sock.close()
+    return c, dc
 
 
-def _make_tlsa_result(host, port, c, dc):
-    result = dict()
-    result['host'] = host
-    result['port'] = port
-    result['cert'] = _get_cert_data(c)
-    result['hash'], result['tlsa'] = _get_tlsa(host, port, dc)
-    result['match'] = 'yes' if result['hash'] == result['tlsa'] else 'no'
-    return result
-
-
-def _get_cert_data(c):
-    result = dict()
-    result['from_d'] = datetime.strptime(c['notBefore'], "%b %d %H:%M:%S %Y %Z")
-    result['to_d'] = datetime.strptime(c['notAfter'], "%b %d %H:%M:%S %Y %Z")
-    result['cn'] = _get_field('commonName', c['subject'])
-    result['an'] = ", ".join([x[1] for x in c['subjectAltName'] if x[0] == 'DNS'])
-    return result
-
-
-def _get_tlsa(host, port, dc):
-    try:
-        a = get_resolver().query("_%s._tcp.%s." % (port, host), 'TLSA')
-        for ans in a:
-            if ans.selector == 0:  # full cert
-                if ans.mtype == 1:  # SHA256
-                    return hashlib.sha256(dc).hexdigest(), binascii.hexlify(a[0].cert).decode()
-                elif ans.mtype == 2:  # SHA512
-                    return hashlib.sha512(dc).hexdigest(), binascii.hexlify(a[0].cert).decode()
-        return 'TLSA ERROR' * 2
-    except dns.resolver.NoAnswer:
-        return 'NO ANSWER' * 2
-    except dns.resolver.NXDOMAIN:
-        return ['NXDOMAIN'] * 2
-
-
-@app.template_global()
-def get_cert_smtp(host, port):
+def _get_cert_smtp(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     s.setblocking(False)
@@ -250,13 +186,13 @@ def get_cert_smtp(host, port):
                     t.join()
                     break
                 elif l.startswith('220'):
-                    s.send(b'EHLO 87-249-184-71.ljusnet.se\r\n')
+                    s.send(("EHLO %s\r\n" % socket.getfqdn()).encode())
                 elif l.startswith('250-STARTTLS'):
                     got_tls = True
 
         s.setblocking(True)
         ssl_context = _get_context()
-        ssl_context.check_hostname = False
+        # ssl_context.check_hostname = False
         ssl_sock = ssl_context.wrap_socket(s)
         c = ssl_sock.getpeercert()
         dc = ssl_sock.getpeercert(binary_form=True)
@@ -265,20 +201,90 @@ def get_cert_smtp(host, port):
         return c, dc
 
 
-@app.template_global()
-def get_cert(host, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssl_context = _get_context()
-    ssl_context.check_hostname = True
-    ssl_sock = ssl_context.wrap_socket(s, server_hostname=host)
-    ssl_sock.connect((host, port))
-    c = ssl_sock.getpeercert()
-    dc = ssl_sock.getpeercert(binary_form=True)
-    ssl_sock.close()
-    return c, dc
+def _get_cert_data(c, dc):
+    def _get_field(s, d):
+        for t in d:
+            if t[0][0] == s:
+                return t[0][1]
+    result = dict()
+    result['from_d'] = datetime.strptime(c['notBefore'], "%b %d %H:%M:%S %Y %Z")
+    result['to_d'] = datetime.strptime(c['notAfter'], "%b %d %H:%M:%S %Y %Z")
+    result['cn'] = _get_field('commonName', c['subject'])
+    result['an'] = ", ".join([x[1] for x in c['subjectAltName'] if x[0] == 'DNS'])
+    result['sha256'] = hashlib.sha256(dc).hexdigest()
+    result['sha512'] = hashlib.sha512(dc).hexdigest()
+    return result
 
 
-# @app.template_global()
+def _get_tlsa(host, port):
+    tlsa = dict()
+    error = list()
+    try:
+        a = _get_resolver().query("_%s._tcp.%s." % (port, host), 'TLSA')
+        tlsa['ad'] = yn('AD' in dns.flags.to_text(a.response.flags))
+        for ans in a:
+            if ans.usage == 3:
+                if ans.selector == 0:  # full cert
+                    if ans.mtype == 1:  # SHA256
+                        if 'sha256' in tlsa:
+                            error.append('MULTIPLE SHA256 RECORDS')
+                        tlsa['sha256'] = binascii.hexlify(ans.cert).decode()
+                    elif ans.mtype == 2:  # SHA512
+                        if 'sha512' in tlsa:
+                            error.append('MULTIPLE SHA512 RECORDS')
+                        tlsa['sha512'] = binascii.hexlify(ans.cert).decode()
+                    else:
+                        error.append('UNKNOWN HASH')
+                else:
+                    error.append('UNKNOWN SELECTOR')
+            else:
+                error.append('UNKNOWN USAGE')
+    except dns.exception.DNSException as e:
+        error.append(str(e))
+
+    if error:
+        tlsa['error'] = error
+
+    return tlsa
+
+
+def _check_tlsa(cert, tlsa):
+    check = dict()
+    check['match_sha256'] = yn('sha256' in tlsa and cert['sha256'] == tlsa['sha256'])
+    check['match_sha512'] = yn('sha512' in tlsa and cert['sha512'] == tlsa['sha512'])
+    return check
+
+
+def _make_https_result(host, port=443):
+    result = dict()
+    result['host'] = host
+    result['port'] = port
+    error = list()
+    try:
+        c, dc = _get_cert(host, port)
+        cert = _get_cert_data(c, dc)
+        result['cert'] = cert
+        tlsa = _get_tlsa(host, port)
+        result['tlsa'] = tlsa
+        result['check'] = _check_tlsa(cert, tlsa)
+    except (socket.error, TimeoutError, ssl.SSLError, ssl.CertificateError, dns.exception.DNSException) as e:
+        error.append(str(e))
+
+    if error:
+        result['error'] = error
+
+    return result
+
+
+def _make_smtp_result(host, port=25):
+    try:
+        c, dc = _get_cert_smtp(host, port)
+    except (socket.error, TimeoutError, ssl.SSLError, ssl.CertificateError, dns.exception.DNSException) as e:
+        return {'host': host, 'port': port, 'error': str(e)}
+
+    return _check_tlsa(host, port, c, dc)
+
+
 def _get_ssh_key(host, port=22):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
@@ -290,27 +296,12 @@ def _get_ssh_key(host, port=22):
     return k
 
 
-def get_resolver():
-    r = dns.resolver.Resolver()
-    r.nameservers = ['8.8.8.8']
-    r.use_edns(0, dns.flags.DO, 1280)
-    return r
-
-
-# @app.template_global()
-def get_tlsa(host, port):
-    try:
-        a = get_resolver().query("_%s._tcp.%s." % (port, host), 'TLSA')
-        return binascii.hexlify(a[0].cert).decode()
-    except dns.resolver.NoAnswer:
-        return 'noanswer'
-
-
-# @app.template_global()
 def _get_sshfp(host):
-    reply = list()
+    reply = dict()
+    rrd = list()
     try:
-        a = get_resolver().query("%s" % host, 'SSHFP')
+        a = _get_resolver().query("%s" % host, 'SSHFP')
+        reply['ad'] = yn('AD' in dns.flags.to_text(a.response.flags))
         for r in a:
             d = dict()
 
@@ -323,8 +314,7 @@ def _get_sshfp(host):
             elif r.algorithm == 4:
                 d['algorithm_type'] = 'ED25519'
             else:
-                d['error'] = 'SSHFP ALGORITHM ERROR (%s)' % r.algorithm
-                reply.append(d)
+                rrd.append({'error': 'SSHFP ALGORITHM ERROR (%s)' % r.algorithm})
                 break
             d['algorithm_code'] = r.algorithm
 
@@ -333,16 +323,16 @@ def _get_sshfp(host):
             elif r.fp_type == 2:
                 d['fingerprint_type'] = 'SHA-256'
             else:
-                d['error'] = 'SSHFP FINGERPRINT ERROR (%s)' % r.fp_type
-                reply.append(d)
+                rrd.append({'error': 'SSHFP FINGERPRINT ERROR (%s)' % r.algorithm})
                 break
             d['fingerprint_code'] = r.fp_type
             d['fingerprint'] = binascii.hexlify(r.fingerprint).decode()
-            reply.append(d)
+            rrd.append(d)
 
     except dns.resolver.NoAnswer as e:
-        return [{'error': str(e)}]
+        return {'error': str(e)}
 
+    reply['rrd'] = rrd
     return reply
 
 
@@ -355,7 +345,7 @@ def _check_sshfp(key, sshfp):
     elif type(key) is pm.ecdsakey.ECDSAKey:
         key_type = 3
     else:
-        return {'error': 'INVALID_KEY_TYPE'}
+        return {'error': 'UNKNOWN_KEY_TYPE'}
 
     d['key_sshfp_type_code'] = key_type
     d['key_type_str'] = key.get_name()
@@ -367,7 +357,7 @@ def _check_sshfp(key, sshfp):
     d['sha256_match'] = ''
 
     fp_found = False
-    for i in sshfp:
+    for i in sshfp['rrd']:
         if 'error' in i:
             continue
         if i['algorithm_code'] == key_type:
@@ -385,9 +375,33 @@ def _check_sshfp(key, sshfp):
     return d
 
 
+def _make_sshfp_result(host, port=22):
+    result = dict()
+    result['host'] = host
+    result['port'] = port
+    error = list()
+    try:
+        k = _get_ssh_key(host, port)
+        sshfp = _get_sshfp(host)
+        result['sshfp'] = sshfp
+        result['check'] = _check_sshfp(k, sshfp)
+    except (TimeoutError, dns.exception.DNSException) as e:
+        error.append(str(e))
+
+    if error:
+        result['error'] = error
+
+    return result
+
+
 @app.template_global()
 def is_none(test):
     return test is None
+
+
+@app.template_global()
+def yn(test):
+    return 'yes' if test else 'no'
 
 
 @app.template_global()
