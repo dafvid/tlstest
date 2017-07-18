bd8ea88bd49aba200fac8907af191de049539626
David Wahlund <david@dafnet.se>
2017-07-18 21:31:07 +0200

    New SMTP-client

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ssltest',
+      version='170718.1',
+      url='http://www.dafnet.se',
+      author='David Wahlund',
+      author_email='david@dafnet.se',
+      packages=['ssltest'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['flask', 'paramiko', 'dnspython']
+      )
